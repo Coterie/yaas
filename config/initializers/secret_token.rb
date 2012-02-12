fc87466d315afeb9751a0bbf7fcf8e5a40bb143c
Ramasamyz <ramasamy.zephyr@gmail.com>
2012-02-12 18:54:34 +0530

    initializing folder structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yaas::Application.config.secret_token = '17b1dbd4341b7acd0d3039e2700796b937c44802717142a3446317911bac358ed162adf8e4479b07559531c225e90c306e6d0bddc5631c838d7223c6ff72ffbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Yaas::Application.config.session_store :cookie_store, key: '_yaas_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Yaas::Application.config.session_store :active_record_store
